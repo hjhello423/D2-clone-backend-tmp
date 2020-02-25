1fb831e2885925f7c31b92107b885b2e48157441
chj <hongjc423@gmail.com>
2020-02-26 00:12:52 +0900

    commit

---
+++ src/test/java/com/clonestudy/d2clone/D2CloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonestudy.d2clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2CloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
